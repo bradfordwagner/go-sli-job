1dbc2f066654b230764477d3edb77d8ed5acf859
bradfordwagner <wagner.bradford@gmail.com>
2024-09-25 15:33:32 -0400

    init

--- pkg/sli/context.go
+++ pkg/sli/context.go
@@ -1,1 +1,25 @@
 package sli
+
+import (
+	"github.com/bradfordwagner/go-kubeclient/kube"
+	"k8s.io/client-go/kubernetes"
+)
+
+// Context is the context for the SLI package
+type Context struct {
+	KubeClient kubernetes.Interface
+	Pusher     PusherInterface
+}
+
+// NewContext creates a new context for the SLI package
+func NewContext() (c *Context, err error) {
+	client, err := kube.Client()
+	if err != nil {
+		return
+	}
+
+	return &Context{
+		KubeClient: client,
+		Pusher:     NewPusher(),
+	}, nil
+}
---
+++ pkg/sli/push.go
@@ -0,0 +1,93 @@
+package sli
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type PushMetric struct {
+	Name        string
+	Description string
+	MetricType  MetricType
+	Value       float64
+
+	//  in the format of key="value"
+	ExtraLabels string
+}
+
+type MetricType string
+
+const (
+	GaugeType   MetricType = "gauge"
+	CounterType MetricType = "counter"
+)
+
+func (m MetricType) String() string {
+	return string(m)
+}
+
+type Pusher struct {
+	Url string
+}
+
+// enforce interface
+var _ PusherInterface = &Pusher{}
+
+func NewPusher() *Pusher {
+	return &Pusher{}
+}
+
+func (p *Pusher) Push(url string, metrics ...PushMetric) error {
+	if len(metrics) == 0 {
+		return nil
+	}
+
+	client := http.Client{}
+
+	buf := bytes.NewBuffer([]byte{})
+	for _, metric := range metrics {
+		if err := writeMetric(buf, metric); err != nil {
+			return err
+		}
+	}
+
+	resp, err := client.Post(url, "text/plain", buf)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return fmt.Errorf(
+			"Got %v response when pushing metrics to %v",
+			resp.StatusCode,
+			p.Url,
+		)
+	}
+
+	return nil
+}
+
+func writeMetric(w io.Writer, metric PushMetric) error {
+	// https://github.com/prometheus/docs/blob/main/content/docs/instrumenting/exposition_formats.md#text-format-example
+	if _, err := fmt.Fprintf(w, "# HELP %s %s\n", metric.Name, metric.Description); err != nil {
+		return err
+	}
+
+	if _, err := fmt.Fprintf(w, "# TYPE %s %s\n", metric.Name, metric.MetricType.String()); err != nil {
+		return err
+	}
+
+	if _, err := fmt.Fprintf(w, "%s{%s} %f\n", metric.Name, metric.ExtraLabels, metric.Value); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type PusherInterface interface {
+	Push(url string, metrics ...PushMetric) error
+}
